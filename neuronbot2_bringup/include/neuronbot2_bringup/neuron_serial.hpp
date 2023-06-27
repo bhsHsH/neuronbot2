4cb8b31847a9bdf511f241939317c40c10fbdbf6
Bernd Schmidt <bernd-hans.schmidt@hs-hannover.de>
2023-06-27 12:06:19 +0200

    tf2_geometry_msgs: .hpp instead of .h required

--- neuronbot2_bringup/include/neuronbot2_bringup/neuron_serial.hpp
+++ neuronbot2_bringup/include/neuronbot2_bringup/neuron_serial.hpp
@@ -28,7 +28,7 @@
 #include <sensor_msgs/msg/imu.hpp>
 #include <sensor_msgs/msg/magnetic_field.hpp>
 #include <tf2/LinearMath/Quaternion.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2_ros/transform_broadcaster.h>
 
 #include "neuronbot2_bringup/dataframe.hpp"
